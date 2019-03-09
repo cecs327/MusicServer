19f8d31e700391bd9298736956cc718b0264331c
kylemazza <kyleamazza@gmail.com>
2019-03-08 19:11:45 -0800

    Added more tests to cover new Dispatcher methods

--- src/test/java/TestDispatch.java
+++ src/test/java/TestDispatch.java
@@ -1,70 +1,115 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import core.Dispatcher;
 import core.LoginDispatcher;
 import core.SongDispatcher;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestDispatch {
-    private static String jsonMusicString;
-    private static String jsonMethodsString;
+    private Dispatcher dispatcher;
+    private static Gson gson;
+
+    private static JsonArray jsonMusicObject;
+    private static JsonObject jsonMethodsObject;
 
     @BeforeAll
-    static void setup() {
-        Gson gson = new Gson();
+    static void init() {
+        gson = new Gson();
 
-        JsonArray jsonMusicObject = gson.fromJson(new InputStreamReader(
+        jsonMusicObject = gson.fromJson(new InputStreamReader(
                 TestDispatch.class.getResourceAsStream("/music.json")),
                 JsonArray.class);
-        JsonArray jsonMethodsObject = gson.fromJson(new InputStreamReader(
+
+        jsonMethodsObject = gson.fromJson(new InputStreamReader(
                 TestDispatch.class.getResourceAsStream("/methods.json")),
-                JsonArray.class);
-        jsonMusicString = jsonMusicObject.toString();
-        jsonMethodsString = jsonMethodsObject.toString();
+                JsonObject.class);
+    }
 
-        assertNotNull(jsonMusicString, "Music JSON string is null.");
-        assertNotNull(jsonMethodsString, "Methods JSON string is null.");
-
-        System.out.println(jsonMusicString);
-        System.out.println(jsonMethodsString);
+    @BeforeEach
+    void setup() {
+        dispatcher = new Dispatcher();
     }
 
     @Test
     void registerDispatcherService() {
-        Dispatcher dispatcher = new Dispatcher();
         dispatcher.registerDispatcher(SongDispatcher.class.getSimpleName(), new SongDispatcher());
 
         assertFalse(dispatcher.getDispatchers().isEmpty());
     }
 
     @Test
-    void loginDispatch() {
-        Dispatcher dispatcher = new Dispatcher();
+    void stringifyJsonToArray_login() {
+        JsonObject loginMethodJson = jsonMethodsObject.get("login").getAsJsonObject();
+        JsonObject paramsObject = loginMethodJson.get("params").getAsJsonObject();
+        paramsObject.addProperty("username", "user");
+        paramsObject.addProperty("password", "pass");
 
-        Gson gson = new Gson();
-        Map<String, Object> request = Map.ofEntries(
-            Map.entry("method", "login"),
-            Map.entry("dispatcher", "LoginDispatcher"),
-            Map.entry("callSemantic", "AT_LEAST_ONCE"),
-            Map.entry("params", Map.ofEntries(
-                        Map.entry("username", "user"),
-                        Map.entry("password", "pass")
-                    )
-            )
-        );
+        String[] params = dispatcher.stringifyParametersToArray(loginMethodJson);
 
-        String json = gson.toJson(request);
-        System.out.println(json);
+        String[] expected = { "user", "pass" };
+
+        assertArrayEquals(params, expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getSongChunkProvider")
+    void stringifyJsonToArray_song(JsonObject songChunkMethodJson) {
+
+        String[] params = dispatcher.stringifyParametersToArray(songChunkMethodJson);
+
+        String[] expected = { "41838", "2" };
+        assertArrayEquals(params, expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getSongChunkProvider")
+    void typifyParameters_getSongChunk(JsonObject songChunkMethodJson) {
+        Class[] types = { Long.class, Integer.class };
+
+        songChunkMethodJson = jsonMethodsObject.get("getSongChunk").getAsJsonObject();
+        String[] params = dispatcher.stringifyParametersToArray(songChunkMethodJson);
+
+        Object[] typedParams = dispatcher.typifyParameters(params, types);
+
+        Object[] expected = { 41838L, 2 };
+        assertArrayEquals(typedParams, expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getSongChunkProvider")
+    void loginDispatch(JsonObject songChunkMethodJson) {
 
         dispatcher.registerDispatcher(LoginDispatcher.class.getSimpleName(), new LoginDispatcher());
+        dispatcher.registerDispatcher(SongDispatcher.class.getSimpleName(), new SongDispatcher());
 
-        String response = dispatcher.dispatch(json);
-        System.out.println(response);
+        String response = dispatcher.dispatch(gson.toJson(songChunkMethodJson));
+
+        assertFalse(response.contains("error"), "Error response object was received.");
+    }
+
+    private static Stream<Arguments> getSongChunkProvider() {
+        JsonObject songChunkMethodJson = jsonMethodsObject.get("getSongChunk").getAsJsonObject();
+        JsonObject paramsObject = songChunkMethodJson.get("params").getAsJsonObject();
+        paramsObject.addProperty("song", "41838");
+        paramsObject.addProperty("fragment", "2");
+
+        return Stream.of(
+            Arguments.of(songChunkMethodJson)
+        );
     }
 }
